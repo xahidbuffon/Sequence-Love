a1880f170543a63e3ae53fa870b98c78dceeaffc
xahid <islam034@umn.edu>
2018-12-24 06:52:13 -0600

    readme updated

--- vizRecaman.py
+++ vizRecaman.py
@@ -2,14 +2,20 @@
 """
 @author: Jahid 
 @email: islam034@umn.edu
+
+visialier for Recaman sequence
 """
+
 from __future__ import division
 from matplotlib import pyplot as plt
 from matplotlib import patches as pat
 
+# for colored arcs
 colors=['blue', 'red', 'green', 'orange', 'pink', 'cyan', 'magenta', 'yellow']
 
 def generateRecaman(N=50):
+    """ generates first N numbers in Recaman sequence
+    """
     seqRecaman = [0]
     for i in range(1, N):
         if (seqRecaman[-1] - i in seqRecaman) or (seqRecaman[-1] - i < 0):
@@ -20,6 +26,8 @@ def generateRecaman(N=50):
 
 
 def getColor(i, colored=False):
+    """ for colored arcs
+    """
     global colors
     if not colored: return 'black'
     else:  return colors[i%8]
@@ -27,8 +35,11 @@ def getColor(i, colored=False):
 
 
 def vizualizeRecaman(N=50, colored=False):
+    """ generates arcs for first N numbers in Recaman sequence
+    """
+    # get the sequence 
     seqRecaman = generateRecaman(N)
-
+    # plot arcs for each number
     fig = plt.figure()
     ax = fig.add_subplot(111)
     for i in range(len(seqRecaman) - 1):
@@ -40,7 +51,6 @@ def vizualizeRecaman(N=50, colored=False):
                              color=getColor(i, colored),
                              linewidth=1.1)
         ax.add_patch(new_patch)
-
     canvas_scale = 1.1
     ax.set_xlim(-(canvas_scale-1)*max(seqRecaman), canvas_scale*max(seqRecaman))
     ax.set_ylim(-0.6*canvas_scale*N, 0.6*canvas_scale*N)
