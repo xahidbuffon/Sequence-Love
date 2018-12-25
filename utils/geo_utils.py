33118384d36c508d7cfc6f342546d62d3ef682b4
xahidbuffon <xahid.buffon@gmail.com>
2018-12-25 02:53:07 -0600

    Fibonacci added

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -7,9 +7,11 @@ Utility functions for geometric computations
 """
 from __future__ import division
 import numpy as np
+import matplotlib.patches as mpatches
+
 
 def get_line_pts2d(p0, p1, n):
-    """ get n interpolated points between o0, p1
+    """ get n interpolated points between p0, p1
     """
     x1, y1 = p0
     x2, y2 = p1 
@@ -25,5 +27,77 @@ def get_line_pts2d(p0, p1, n):
     for i in xrange(n):
         ys = slope*(Xs[i]-x1) + y1
         pts.append([Xs[i], ys])
+
     return pts
         
+
+
+
+def getSpiralGoldenRatio(N=200):
+    """ Finds patches of spirals based on golden ratio
+    """
+    cx = cy = 0.5 # centre of spiral
+    spacing = 0.1 # inter-spiral
+
+    golden_ratio = (1 + 5**0.5) / 2.0 # approx = 1.618
+    delta = (2 - golden_ratio) * 2 * np.pi + spacing
+
+    scale = 0.1
+    leaf_rad= scale * (1 + 5**0.5)/4.0 # radius of each leaf
+    theta=0; min_y = max_y = 0;
+
+    # collect list of spirals
+    patches = []
+    for i in xrange(1, N+1):
+        r = scale * i **0.5
+        theta += delta
+        x = cx + r*np.cos(theta)
+        y = cy + r*np.sin(theta)
+        if   y > max_y: max_y = y
+        elif y < min_y: min_y = y
+        leaf = mpatches.Circle((x, y), leaf_rad)
+        patches.append(leaf)
+
+    return patches
+
+
+
+
+def getSpiralFibonacci(seqFibo):
+    """ Finds patches of spirals based on the given Fibonacci sequence
+    """
+    c = xy = np.array([0, 0])
+    t = np.array([270, 0]) 
+
+    i=1; patches = []
+    for j in xrange(1, len(seqFibo)):
+        t += 90
+        if i==5: 
+            i = 1
+        if i==1:
+            xy = xy + [-seqFibo[j-2], seqFibo[j-1]]
+            c  = c  + [-seqFibo[j-2],  0      ]
+        if i==2:
+            xy = xy + [ -seqFibo[j], -seqFibo[j-2]]     
+            c  =  c + [     0 ,     -seqFibo[j-2]]
+        elif i==3:
+            xy = xy + [   0,        -seqFibo[j]  ]    
+            c  = c  + [seqFibo[j-2],     0       ]
+        elif i==4:
+            xy = xy + [ seqFibo[j-1],      0     ]   
+            c  = c  + [   0,       seqFibo[j-2]  ]
+        else: pass
+
+        # The spiral and the rectangle
+        rect = mpatches.Wedge([c[0], c[1]], seqFibo[j], t[0], t[1])
+        patches.append(rect)
+        rect = mpatches.Rectangle([xy[0], xy[1]], seqFibo[j], seqFibo[j])
+        patches.append(rect)
+        i+=1
+
+    return patches
+
+
+
+
+
---
+++ vizFibonacci.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+@author: Jahid 
+@email: islam034@umn.edu
+
+visialier for Fibonacci sequence
+"""
+from __future__ import division
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+
+
+def generateFibonacci(N=5):
+    """ generates first N numbers in Fibonacci sequence
+    """
+    assert (N>0), "N must be a positive integer"
+    seq = []
+    for i in xrange(N):
+        if i < 2: Pi = i 
+        else: Pi = seq[i-1] + seq[i-2]
+        seq.append(Pi)
+    return seq
+
+
+
+def vizualizeGoldenRatioSpiral(N=100):
+    """ generates Fibonacci sequence and spiral (golden ratio)
+    """
+    seqFibo = generateFibonacci(N)
+    #print ("Fibonacci sequence [{0}]: \n{1}".format(N, seqFibo))
+
+    # Get the spirals
+    from utils.geo_utils import getSpiralGoldenRatio
+    spirals = getSpiralGoldenRatio(N)
+
+    # Plot the spirals
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  
+    Ncolors = 20; colors=[]
+    for j in range(N):
+        colors.append((j % Ncolors) * 5) # choose a color
+        collection = PatchCollection(spirals, cmap=matplotlib.cm.jet, alpha=0.5)
+        collection.set_array(np.array(colors))
+        # add the spiral to collection
+        ax.add_collection(collection)
+
+    plt.axis('equal')
+    plt.savefig('data/Fibo_spiral.png')
+    plt.show()
+
+
+
+
+def vizualizeFibonacciRectSpiral(N=15):
+    """ generates Fibonacci sequence and spiral (golden ratio)
+    """
+    seqFibo = generateFibonacci(N)
+    #print ("Fibonacci sequence [{0}]: \n{1}".format(N, seqFibo))
+
+    # Get the spirals
+    from utils.geo_utils import getSpiralFibonacci
+    spirals = getSpiralFibonacci(seqFibo)
+
+    # Plot the spirals
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  
+    Ncolors = 10; colors=[]
+    for j in range(N):
+        colors.append((j % Ncolors) * 5) # choose a color
+        collection = PatchCollection(spirals, cmap=matplotlib.cm.jet, alpha=0.05)
+        collection.set_array(np.array(colors))
+        # add the spiral to collection
+        ax.add_collection(collection)
+
+    plt.axis('equal')
+    plt.savefig('data/Fibo_spiral_rect.png')
+    plt.show()
+
+
+
+
+N=35
+vizualizeFibonacciRectSpiral(N)
+
+#N=100
+#vizualizeGoldenRatioSpiral(N)
+
+
+
+
+
